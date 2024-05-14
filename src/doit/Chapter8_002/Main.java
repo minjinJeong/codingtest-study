014c2d503fadb03178f5cff9f91fa695a57bc7be
minjinJeong <alswls4390@naver.com>
2024-05-14 19:42:41 +0900

    선택 정렬 문제

--- src/doit/Chapter8_002/Main.java
+++ src/doit/Chapter8_002/Main.java
@@ -7,8 +7,8 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // answer();
-        mySource();
+        answer();
+        // mySource();
     }
     
     private static void mySource() throws IOException {
@@ -47,5 +47,32 @@ public class Main {
     }
     
     private static void answer() throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        int A[] = new int[str.length()];
+
+        for (int i = 0; i < str.length(); i++) {
+            A[i] = Integer.parseInt(str.substring(i, i+1));
+        }
+
+        // 선택정렬
+        for (int i = 0; i < str.length(); i++) {
+            int Max = i;
+            for (int j = i+1; j < str.length(); j++) {
+                if (A[j] > A[Max]) {
+                    Max = j;
+                }
+            }
+            if(A[i] < A[Max]) {
+                int temp = A[i];
+                A[i] = A[Max];
+                A[Max] = temp;
+            }
+        }
+
+        for (int i = 0; i < A.length; i++) {
+            System.out.print(A[i]);
+        }
     }
 }
