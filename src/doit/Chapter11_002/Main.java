623f2a420308ddd4d42e19d909191439569668aa
minjinJeong <alswls4390@naver.com>
2024-05-19 00:07:21 +0900

    오일러 피 함수 추가

--- src/doit/Chapter11_002/Main.java
+++ src/doit/Chapter11_002/Main.java
@@ -12,30 +12,53 @@ public class Main {
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        long N = Long.parseLong(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        System.out.println(M +" 의 서로소는 " + Euler(M));
 
-        long result = Euler(N);
-        System.out.println(result);
+        int N = Integer.parseInt(st.nextToken());
+        Euler(M, N);
     }
     
-    private static long Euler(Long N) {
-        
-        long res = N;
+    private static void Euler(int M, int N) {
 
-        // 2부터 N의 제곱근까지 반복
-        for(long i=2; i <= Math.sqrt(i); i++){
-            if(N%i == 0){
+        int[] A = new int[N+1];
+        int[] res = new int[N+1];
+
+        // 초기화
+        for (int i = 1; i <= N; i++) {
+            A[i] = i;
+            res[i] = i;
+        }
+
+        for (int i = 2; i <= N; i++) {
+            if(res[i] == i) { // 소수일 경우
+                for (int j = i; j <= N; j=j+i) {
+                    res[j] = res[j] - res[j] / i;
+                }
+            }
+        }
+
+        // 출력
+        for (int i = M; i <= N; i++) {
+            System.out.println(res[i]);
+        }
+    }
+    
+    private static int Euler(int N) {
+        
+        int res = N;
+        for(int i=2; i <= Math.sqrt(N); i++){
+            if(N % i == 0) {
                 res = res - res/i;
                 while(N%i == 0) {
                     N /= i;
                 }
             }
         }
-
-        if(N>1) {
+        if(N > 1) {
             res = res - res/N;
         }
-        
+
         return res;
     }
 }
