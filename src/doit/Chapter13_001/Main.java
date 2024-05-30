d59bb6b32bc92866325b7a2307e8dbabedec8502
minjinJeong <alswls4390@naver.com>
2024-05-30 20:22:41 +0900

    백준 11438 LCA 2 풀이 진행 중

--- src/doit/Chapter13_001/Main.java
+++ src/doit/Chapter13_001/Main.java
@@ -14,25 +14,46 @@ public class Main {
         mySource();
     }
 
-    static int[] unionFind;
+    static int[][] array;
     private static void mySource() throws IOException {
 
         BufferedReader br = 
             new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int N = Integer.parseInt(st.nextToken());
 
         // 초기화
-        int[][] array = new int[N+1][2];
-        for (int i = 0; i < N+1; i++) {
+        array = new int[3][N+1];
+        for (int i = 0; i < N-1; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            array[i][0] = a;
-            array[i][1] = b;
+            array[0][b] = a;
         }
+
+        for (int i = 0; i < array.length; i++) {
+            parent(2, i);
+        }
+
+        // 구하기
+        st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+        
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            System.out.println(array[a][b]);
+        }
+    }
+
+    private static int parent(int K, int N) {
+        if (K == 0) {
+            return array[K][array[K][N]];
+        }
+        return array[K][N] = parent(K-1, N);
     }
 }
